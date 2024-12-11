ab02631c56dd8a6267412e1a5b99c9a3fe5b4745
Peter Vahlberg <peter@vahlberg.org>
2024-12-11 10:43:25 -0800

    Next commit

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
 
-import MurderQuiz from "./MurderQuiz";
+import MurderQuiz from "./MurderQuiz"
 
 
-function main() {
-  console.log("Loaded");
+
+console.log("Loaded");
 
 
-  let newQuiz = new MurderQuiz();
-  
-  // References
-  choiceField = document.getElementById("choice-set");
-  title = document.getElementById("title");
-  textField = document.getElementById("text-area");
-  elseBox = document.getElementById("else-box");
-  enterButton = document.getElementById("enter-button")
-  
-  enterButton.style.visibility = "hidden";
-  
-  // Choices
-  let choiceList = ["Money", "True Love", "Power"]
-  choiceList.forEach(choice => {
-    let newButton = document.createElement("button");
-    newButton.textContent = choice;
-    newButton.className = "choice";
-    newButton.addEventListener("click", function() {
-      enterData(choice);
-    });
-    choiceField.insertBefore(newButton, textField);
-    console.log(choice);
+let newQuiz = new MurderQuiz();
+
+// References
+choiceField = document.getElementById("choice-set");
+title = document.getElementById("title");
+textField = document.getElementById("text-area");
+elseBox = document.getElementById("else-box");
+enterButton = document.getElementById("enter-button")
+
+enterButton.style.visibility = "hidden";
+
+// Choices
+let choiceList = ["Money", "True Love", "Power"]
+choiceList.forEach(choice => {
+  let newButton = document.createElement("button");
+  newButton.textContent = choice;
+  newButton.className = "choice";
+  newButton.addEventListener("click", function() {
+    enterData(choice);
   });
-  
-  
-  elseBox.addEventListener("change", function() {
-    console.log(elseBox.value);
-    if (elseBox.value.length > 1) {
-      enterButton.style.visibility = "";
-    } else {
-      enterButton.style.visibility = "hidden";
-    }
-  });
-  
-  enterButton.addEventListener("click", function() {
-    val = elseBox.value;
-    enterData(val);
-  });
-  
-  
-  function enterData(data) {
-    const formData = {
-      answer: data
-    };
-    
-    fetch('https://fabform.io/f/WgtpQaL', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success === "true") {
-        console.log("Success!");
-      } else {
-        throw new Error('Server response indicates failure');
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  
-    document.getElementById("mainBody").removeChild(choiceField);
-    title.textContent = "Thanks for your answer!";
+  choiceField.insertBefore(newButton, textField);
+  console.log(choice);
+});
+
+
+elseBox.addEventListener("change", function() {
+  console.log(elseBox.value);
+  if (elseBox.value.length > 1) {
+    enterButton.style.visibility = "";
+  } else {
+    enterButton.style.visibility = "hidden";
   }
-    
-}
+});
 
-export default main;
+enterButton.addEventListener("click", function() {
+  val = elseBox.value;
+  enterData(val);
+});
+
+
+function enterData(data) {
+  const formData = {
+    answer: data
+  };
+  
+  fetch('https://fabform.io/f/WgtpQaL', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success === "true") {
+      console.log("Success!");
+    } else {
+      throw new Error('Server response indicates failure');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  document.getElementById("mainBody").removeChild(choiceField);
+  title.textContent = "Thanks for your answer!";
+}
+  
+
+
+
