85978aa36659000b653fe2ba0c78de14ef55a051
Peter Vahlberg <peter@vahlberg.org>
2024-12-11 10:24:43 -0800

    Initial commit

---
+++ MurderQuiz.js
@@ -0,0 +1,62 @@
+
+
+class MurderQuiz {
+  constructor() {
+    this.questionIndex = 0;
+
+    this.poison = 0;
+    this.hammer = 0;
+  }
+
+
+  quiz = [
+  {
+    title: "How do you generally prefer to enjoy things?",
+    questions: [
+      {
+        text: "Slowly",
+        img: "",
+        action: () => {
+          this.poison += 1;
+        }
+      },
+      {
+        text: "Quickly",
+        img: "",
+        action: () => {
+          this.hammer += 1;
+        }
+      }
+    ]
+  },
+  {
+    title: "Second Question",
+    questions: [
+      {
+        text: "One",
+        img: "",
+        action: () => {
+          this.poison += 1;
+        }
+      },
+      {
+        text: "Two",
+        img: "",
+        action: () => {
+          this.hammer += 1;
+        }
+      }
+    ]
+  }
+  ]
+
+
+  nextQuestion() {
+    return this.quiz[this.questionIndex++];
+  }
+  
+
+
+}
+
+export default MurderQuiz;
